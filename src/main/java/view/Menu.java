d935eb8167a49a2b4dc5176b612c06a91b636d04
Magda Wołk <wolk.mm@gmail.com>
2021-03-21 12:53:07 +0100

    Menu komunikacji z użytkownikiem

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -3,8 +3,6 @@ package view;
 
 import lombok.extern.log4j.Log4j;
 import org.apache.log4j.Logger;
-
-import java.util.InputMismatchException;
 import java.util.Scanner;
 @Log4j
 public class Menu {
@@ -12,26 +10,25 @@ public class Menu {
     private final static Scanner scanner = new Scanner(System.in);
     private final static Logger logger = Logger.getLogger(Menu.class);
 
-
     public static void userConsole() {
-        int option;
-        try {
+        String option;
+
             displayMenu();
-            option = scanner.nextInt();
+            option = scanner.nextLine();
             switch (option) {
-                case 1:
+                case "1":
                     // uzupełnić
                     logger.info("Dodano lokalizację do bazy danych");
                     break;
-                case 2:
+                case "2":
                     //uzupełnić
                     logger.info("Wyświetlono wszystkie lokalizacje z bazy danych");
                     break;
-                case 3:
+                case "3":
                     //uzupełnić
                     logger.info("Pobrano dane pogodowe");
                     break;
-                case 4:
+                case "4":
                     logger.info("Zamknięto aplikację");
                     System.exit(0);
                     break;
@@ -39,12 +36,42 @@ public class Menu {
                     logger.info("Wprowadzono błędną wartość. Brak takiej opcji w menu.");
                     userConsole();
             }
-        } catch (InputMismatchException e) {
-            logger.error("Wprowadzono wartość nie będącą liczbą");
-            scanner.nextLine();
-            userConsole();
-        }
+
     }
+
+//    metoda userConsole() pobierająca od użytkownika inty
+//    public static void userConsole() {
+//        int option;
+//        try {
+//            displayMenu();
+//            option = scanner.nextInt();
+//            switch (option) {
+//                case 1:
+//                    // uzupełnić
+//                    logger.info("Dodano lokalizację do bazy danych");
+//                    break;
+//                case 2:
+//                    //uzupełnić
+//                    logger.info("Wyświetlono wszystkie lokalizacje z bazy danych");
+//                    break;
+//                case 3:
+//                    //uzupełnić
+//                    logger.info("Pobrano dane pogodowe");
+//                    break;
+//                case 4:
+//                    logger.info("Zamknięto aplikację");
+//                    System.exit(0);
+//                    break;
+//                default:
+//                    logger.info("Wprowadzono błędną wartość. Brak takiej opcji w menu.");
+//                    userConsole();
+//            }
+//        } catch (InputMismatchException e) {
+//            logger.error("Wprowadzono wartość nie będącą liczbą");
+//            scanner.nextLine();
+//            userConsole();
+//        }
+//    }
 
     public static void displayMenu(){
         System.out.println("************MENU************");
